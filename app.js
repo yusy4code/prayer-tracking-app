97fecbbf4dc218132cafdcc10f5d6e8be8dd2bc5
Yusuf <yusuf.matrix2008@gmail.com>
2024-12-22 12:13:59 +0400

    Adding first route

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 import express from 'express';
+import 'dotenv/config'
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
+
+app.get('/', (req, res) => {
+  return res.status(200).json({ success: true });
+});
 
 app.listen(PORT, () => console.log(`App started on port ${PORT}`));
