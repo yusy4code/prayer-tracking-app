adaa453dcd29ed87049a506c82458a17bd6252c4
Yusuf <yusuf.matrix2008@gmail.com>
2024-12-29 20:09:29 +0400

    Using react dist folder

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import 'dotenv/config';
 import path from 'path';
 import express from 'express';
 import morgan from 'morgan';
+import cors from 'cors';
 import { LOGGER_FORMAT } from './utils/constants.js';
 import apiController from './controller/apiController.js';
 import connectToDatabase from './config/db.js';
@@ -14,22 +15,20 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+app.use(cors({ origin: 'http://localhost:4173' }));
 app.use(morgan(LOGGER_FORMAT));
 
 // API routes
 app.use('/api/v1', apiController);
 
 // Serve static files (React/Vue/Angular front-end build)
-app.use(express.static(path.join(path.resolve(), 'public')));
+app.use(express.static(path.join(path.resolve(), 'dist')));
 
 // Catch-all route to serve index.html for front-end
 app.get('*', (req, res) => {
-  res.sendFile(path.join(path.resolve(), 'public/index.html'));
+  res.sendFile(path.join(path.resolve(), 'dist/index.html'));
 });
 
-// app.get('/', (req, res) => {
-//   return res.status(200).json({ success: true });
-// });
 
 connectToDatabase().then(() => {
   app.listen(PORT, () => console.log(`App started on port ${PORT}`));
